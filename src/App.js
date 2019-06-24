ecec1f18d3cadbfe0527ff6f97a48f1f6f3bab73
john2796 <jbmiranda22796@gmail.com>
2019-06-24 00:00:07 -0700

    added todo list next thing todo

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import ProjectContainer from './components/projects/ProjectContainer';
 import BlueCircles from './components/main-header/BlueCircles';
 import Footer from './components/footer/Footer';
 // things todo :
-// -[] complete udemy course about hooks first then comback
-// -[] change Lorem ipsum words
-// -[] Update react icon to be same as logo
 // -[] Add smooth scroll (do this last)
 // -[] Add a loading
+// -[] Add a way to see resume
+// -[x] change Lorem ipsum words
+// -[x] Update react icon to be same as logo
 // -[x] Only have mail github linkedIn on socials
 // -[x] connect social media link and decide what todo with my name on left side.
 // -[x] Add github links to the project
